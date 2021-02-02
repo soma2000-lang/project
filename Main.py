4e47aeb145b906f29e5ffc3eb04be6a25dbda364
Somasree Majumder <bishnupadamajumder32@gmail.com>
2021-02-02 14:26:09 +0530

    I have added a python file

---
+++ Main.py
@@ -0,0 +1,9 @@
+from Edge import Edge
+from UtilityFunctions import shortestPaths
+from UtilityFunctions import readRouters
+from collections import defaultdict
+
+
+adjList = defaultdict(list)
+# Node 0 edges 0-1 with weight 10, 0-2 with weight 3
+
